47d3db46b22366654c81b626c95f8f386ccbd075
Wei C <69275472+Vivi202008@users.noreply.github.com>
2021-06-14 11:30:03 +0200

    Update Program.cs
    1. Nth root, Nth power
    2. Try to find expetion:
    - Divisor cannot be 0
    - Exponent operation exponent integer, exponent and base cannot be 0 at the same time
    -  the root operation, the root n is a positive integer

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
 
 namespace Calculator
 
 {
     //Basic calculator
-
+    
     class Calculator
     {
         //This is a basic calculator that is able to perform basic mathematical operations. + - X /
-        static void Main(string[] args)
+        public double number1=0, number2=0, result=0;
+        public string operatorString="";
+        public void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to my calculator");
             
@@ -22,38 +24,47 @@ namespace Calculator
                 Console.WriteLine("2: Subtraction.");
                 Console.WriteLine("3: Multiplication.");
                 Console.WriteLine("4: Division.");
-                Console.WriteLine("5: Square root.");
-                Console.WriteLine("6: Raised to 2.");
-                Console.WriteLine("9: Quit.");
+                Console.WriteLine("5: Root.");
+                Console.WriteLine("6: Exponentiation..");
+
+                Console.WriteLine("0: Quit.");
                 Console.WriteLine();
                 userSelection = Convert.ToInt32(GetNumberFromUser("Menu selection"));
                 switch (userSelection)
                 {
                     case 1:
-                        Console.WriteLine("1: Addition.\n  Input 2 numbers.");
+                        Console.WriteLine("1: Addition.\tInput 2 numbers.");
+                        Input2numbers();
                         Add();
                         break;
                     case 2:
-                        Console.WriteLine("2: Subtraction.\n  Input 2 numbers.");
+                        Console.WriteLine("2: Subtraction.\tInput 2 numbers.");
+                        Input2numbers();
                         Subtract();
                         break;
                     case 3:
-                        Console.WriteLine("3: Multiplication.\n  Input 2 numbers.");
+                        Console.WriteLine("3: Multiplication.\tInput 2 numbers.");
+                        Input2numbers(); 
                         Multiply();
                         break;
                     case 4:
-                        Console.WriteLine("4: Division.\n  Input 2 numbers.");
+                        Console.WriteLine("4: Division.\tInput 2 numbers.");
+                        Input2numbers(); 
                         Divide();
                         break;
                     case 5:
-                        Console.WriteLine("5: Square root.\n  Input 1 number.");
+                        Console.WriteLine("5: Root. The nth root of a number\tInput 2 number.");
+                        Input2numbers();
                         Root();
                         break;
                     case 6:
-                        Console.WriteLine("6.Exponentiation. Raised to 2.\n  Input 1 number.");
+                        Console.WriteLine("6.Exponentiation. Raised to the power of n.\tInput 2 number.");
+                        Input2numbers();
                         Exponet();
                         break;
-                    case 9:
+                 
+                                  
+                    case 0:
                         Console.WriteLine("Thanks for using this program.");
                         Console.WriteLine("Press any key to close program.");
                         Console.ReadKey();
@@ -62,61 +73,82 @@ namespace Calculator
                         Console.WriteLine("Not a valid selection.");
                         break;
                 }
-            } while (userSelection != 9);
+            } while (userSelection != 0);
+
+            printResult();
         }  //End of Main
-        static void Add()
+
+        public void Add()
         {
-            double number1 = GetNumberFromUser("first");
-            double number2 = GetNumberFromUser("second");
-            double result = number1 + number2;
-            Console.WriteLine("{0}+{1}={2}", number1, number2, result);
+            result = number1 + number2;
+            operatorString = "+";
         }
-        static void Subtract()
+        public void Input2numbers()
         {
-            double number1 = GetNumberFromUser("first");
-            double number2 = GetNumberFromUser("second");
-            double result = number1 - number2;
-            Console.WriteLine("{0}-{1}={2}", number1, number2, result);
+            number1 = GetNumberFromUser("first");
+            number2 = GetNumberFromUser("second");
         }
 
-        static void Multiply()
+         void Subtract()
         {
-            double number1 = GetNumberFromUser("first");
-            double number2 = GetNumberFromUser("second");
-            double result = number1 * number2;
-            Console.WriteLine("{0}x{1}={2}", number1, number2, result);
+            result = number1 - number2;
+            operatorString = "-";
         }
 
-        static void Divide()
+         void Multiply()
         {
-            double number1 = GetNumberFromUser("first");
-            double number2;
-            do
-            {
+            result = number1 * number2;
+            operatorString = "*";
+        }
+
+        void printResult() {
+            Console.WriteLine("{0} {1} {2} = {3}", number1, operatorString, number2, result);
+        }
+
+         void Divide()
+        {
+            //Divisor cannot be 0       
+                while (number2 == 0)
+                {
+                    Console.WriteLine("Dividtion !! Divisor cannot be 0! Enter other number again!");
+                    number2 =GetNumberFromUser("second");
+                }
+ 
+            result = number1 / number2;
+            operatorString = "/";
+        }
+
+         void Root()
+        {
+            while (number2 < 2 || number2 != Math.Round(number2)) ;
+            {   //Nth root, n>=2
+                 Console.WriteLine(" !! The root number must be a integer that more than 1  ! Enter other number again!");
+                 number2 = GetNumberFromUser("second");
+             } 
+
+            result = Math.Pow(number1,1/number2);
+            operatorString = " root ";
+        }
+
+         void Exponet()
+        {
+            while (number2 != Math.Round(number2)) 
+            {   //Nth power, n is a integer
+                Console.WriteLine(" !! The exponent must be a integer that more than 1  ! Enter other number again!");
                 number2 = GetNumberFromUser("second");
-                if (number2 == 0)
-                    Console.WriteLine("Dividtion !! The second number must not be 0! Enter other number again!");
-            } while (number2 == 0);
 
+            } 
 
-            double result = number1 * number2;
-            Console.WriteLine("{0}x{1}={2}", number1, number2, result);
+            while (number2 ==0 && number1==0) 
+            {   //they can't be 0---exponetion
+                  Console.WriteLine(" The exponent and base cannot be 0 at the same time! Enter other number again!");
+                    number1 = GetNumberFromUser("first");
+            } 
+
+            result = Math.Pow(number1, number2);
+           operatorString=" Raise the power of ";
         }
-
-        static void Root()
-        {
-            double number = GetNumberFromUser("a");
-            double result = Math.Sqrt(number);
-            Console.WriteLine("{0} Square root: {1}", number, result);
-        }
-
-        static void Exponet()
-        {
-            double number = GetNumberFromUser("a");
-            double result = Math.Pow(number, 2);
-            Console.WriteLine("{0} Power of 2: {1}", number, result);
-        }
-        static double GetNumberFromUser(string forWhat)
+        public double GetNumberFromUser(string forWhat)
         {
             bool inputRight = false;
             double number = 0;
